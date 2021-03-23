ee7797b7135d9c0806360ffa84de714df365b4a3
maxtnt <maxtnuk@gmail.com>
2021-03-23 14:11:01 +0900

    onnx update

---
+++ onnx/src/ops/logic.rs
@@ -0,0 +1,18 @@
+use crate::model::OnnxOpRegister;
+use tract_hir::internal::*;
+use tract_hir::ops;
+
+pub fn register_all_ops(reg: &mut OnnxOpRegister) {
+    reg.insert("Not", |_, _| Ok((Box::new(ops::logic::not()), vec![])));
+    reg.insert("And", |_, _| Ok((ops::logic::And.into_hir(), vec![])));
+    reg.insert("Or", |_, _| Ok((ops::logic::Or.into_hir(), vec![])));
+    reg.insert("Xor", |_, _| Ok((ops::logic::Xor.into_hir(), vec![])));
+
+    reg.insert("Equal", |_, _| Ok((ops::logic::Equals.into_hir(), vec![])));
+    reg.insert("Greater", |_, _| Ok((ops::logic::Greater.into_hir(), vec![])));
+    reg.insert("Less", |_, _| Ok((ops::logic::Lesser.into_hir(), vec![])));
+    reg.insert("LessOrEqual", |_, _| Ok((ops::logic::LesserEqual.into_hir(), vec![])));
+    reg.insert("GreaterOrEqual", |_, _| Ok((ops::logic::GreaterEqual.into_hir(), vec![])));
+
+    reg.insert("Where", |_, _| Ok((Box::new(ops::logic::Iff::default()), vec![])));
+}
