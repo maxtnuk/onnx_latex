223eeaba928bc03c0cd139fa9802cdad945b9c83
maxtnt <maxtnuk@gmail.com>
2021-05-03 14:50:48 +0900

    add cnn but not stable

---
+++ latex_front/src/pages/MainPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import MainWraper from "../components/MainWraper"
+
+function MainPage() { 
+  return (
+    <>
+      <MainWraper></MainWraper>
+    </>
+  );
+}
+export default MainPage;
---
+++ latex_front/src/pages/ModelPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ModelPage() {
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default ModelPage;
